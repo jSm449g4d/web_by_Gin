7cda19c1b2542b085b4e95370c628f2d18cdab71
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-12-17 07:13:57 +0900

    Add files via upload

--- gin/22.go
+++ gin/22.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/mem"
 )
 
 func init() {
 	restdict["22.go"] = func(c *gin.Context) {
 		v, _ := mem.VirtualMemory()
-		//cpup, _ := cpu.Percent(0, true)
+		cpup, _ := cpu.Percent(0, true)
 		//cpuf, _ := cpu.Info()
 		c.HTML(200, "22.html", gin.H{
 			//"CPU_M": cpuf[0].ModelName,
 			//"CPU_F": cpuf[0].Mhz,
-			//"CPU_P": cpup,
+			"CPU_P": cpup,
 			"MEM_T": v.Total,
 			"MEM_U": v.Used,
 		})
