29e218ce96f22fbee0a52dcfc93a82b3b9d36186
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-12-17 07:19:24 +0900

    Add files via upload

--- gin/22.go
+++ gin/22.go
@@ -10,10 +10,10 @@ func init() {
 	restdict["22.go"] = func(c *gin.Context) {
 		v, _ := mem.VirtualMemory()
 		cpup, _ := cpu.Percent(0, true)
-		//cpuf, _ := cpu.Info()
+		cpuf, _ := cpu.Info()
 		c.HTML(200, "22.html", gin.H{
 			//"CPU_M": cpuf[0].ModelName,
-			//"CPU_F": cpuf[0].Mhz,
+			"CPU_F": cpuf[0].Mhz,
 			"CPU_P": cpup,
 			"MEM_T": v.Total,
 			"MEM_U": v.Used,
