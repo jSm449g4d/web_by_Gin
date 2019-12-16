99c63666ba3b808f74bab0be087c070273c3ebd6
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-12-17 07:32:03 +0900

    Add files via upload

--- gin/22.go
+++ gin/22.go
@@ -12,8 +12,8 @@ func init() {
 		cpup, _ := cpu.Percent(0, true)
 		cpuf, _ := cpu.Info()
 		c.HTML(200, "22.html", gin.H{
-			//"CPU_M": cpuf[0].ModelName,
-			"CPU_F": cpuf[0].Mhz,
+			"CPU_M": cpuf[0].ModelName,
+			//"CPU_F": cpuf[0].Mhz,
 			"CPU_P": cpup,
 			"MEM_T": v.Total,
 			"MEM_U": v.Used,
